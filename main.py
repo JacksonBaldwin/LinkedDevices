f3001642fbab60d7c0c94577fc3d6ee20719f0cd
Jackson Baldwin <jackson.c.baldwin@gmail.com>
2024-09-25 11:41:30 -0400

    clarity

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ mode = input('setup mode: ')
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     if mode == 'h': #run program as host, establish socket
         s.bind((HOST,PORT))
-        print('bounded to ' + str((HOST,PORT)) + ', waiting for connection...')
+        print('bound to ' + str((HOST,PORT)) + ', waiting for connection...')
         s.listen()
         conn, addr = s.accept()
 
@@ -19,12 +19,11 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             while True:
                 data = conn.recv(1024)
                 print('user:', data.decode())
-                response = input('input message: ')
-                conn.sendall(response.encode())
+                conn.sendall(input('you: ').encode())
 
     if mode == 'c': #run program as client
         s.connect((HOST,PORT))
         while True:
             s.sendall(input('you: ').encode())
             data = s.recv(1024)
-            print('host: '+data.decode())
+            print('user: '+data.decode())
